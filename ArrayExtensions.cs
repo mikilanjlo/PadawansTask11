0d4306b1d62c81a9da0c94baada0d4caa80baf13
EPAM RD NET Lab, Minsk <31510338+EPM-RD-NETLAB@users.noreply.github.com>
2019-02-04 20:49:00 +0300

    Add project files.

---
+++ ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PadawansTask11
+{
+    public static class ArrayExtensions
+    {
+        public static int? FindIndex(double[] array, double accuracy)
+        {
+            // put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
