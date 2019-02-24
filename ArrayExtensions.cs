d53dcd8aa200b68d5404400a72ed4f4680f6bdf1
mikilanjlo <mikilanjlo@yandex.by>
2019-02-24 10:28:08 +0300

    calculate

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,33 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length < 2)
+                throw new ArgumentException();
+            double sumAllElements = 0;
+            foreach (double number in array)
+                try
+                {
+                    sumAllElements =checked(sumAllElements + number);
+                }
+                catch(OverflowException ex)
+                {
+                    throw new OverflowException();
+                }
+            double sumLeftElements = array[0];
+            int ind = -2;
+            for (int i = 1; i < array.Length - 2; i++) {
+                if (Math.Abs(sumLeftElements - (sumAllElements - sumLeftElements - array[i])) < accuracy)
+                {
+                    ind = i;
+                    break;
+                }
+                sumLeftElements += array[i];
+            }
+            if (ind == -2)
+                return null;
+            return ind;
         }
     }
 }
